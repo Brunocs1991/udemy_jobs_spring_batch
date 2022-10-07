9ddbba1b8a3b27194301e39e800017447dfa0895
Bruno <brunocosta.1991@hotmail.com>
2022-10-07 20:26:13 -0300

    Framework Spring Batch

---
+++ 01_projeto_batch/src/main/java/br/com/brunocs/springbatch/config/BatchConfig.java
@@ -0,0 +1,44 @@
+package br.com.brunocs.springbatch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job imprimeOlaJob() {
+        return jobBuilderFactory
+                .get("imprimeOlaJob")
+                .start(imprimeOlaStep())
+                .build();
+    }
+
+    public Step imprimeOlaStep() {
+        return stepBuilderFactory
+                .get("imprimeOlaStep")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Olá mundo");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+}
