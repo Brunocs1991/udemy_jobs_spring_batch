21e7fa19757d0a8d7fd67da24c4403c3b3cca7a2
Bruno <brunocosta.1991@hotmail.com>
2022-10-08 12:44:08 -0300

    pratica multimplas execuções de escopo

--- 01_projeto_batch/src/main/java/br/com/brunocs/springbatch/config/BatchConfig.java
+++ 01_projeto_batch/src/main/java/br/com/brunocs/springbatch/config/BatchConfig.java
@@ -7,9 +7,12 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,18 +30,25 @@ public class BatchConfig {
         return jobBuilderFactory
                 .get("imprimeOlaJob")
                 .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     public Step imprimeOlaStep() {
         return stepBuilderFactory
                 .get("imprimeOlaStep")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Olá mundo");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(imprimeOlaTasklet(null)).build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Olá %s !", nome));
+                return RepeatStatus.FINISHED;
+            }
+        };
     }
 }
